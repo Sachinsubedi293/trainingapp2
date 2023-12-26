88e8515f858913fd871f92af23db7a232d1e064d
Sachinsubedi293 <sachinsubedi616@gmail.com>
2023-12-26 07:39:52 +0545

    first

---
+++ lib/Loginpage.dart
@@ -0,0 +1,79 @@
+
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+import 'package:terainingapp2/Homepage.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+ class _LoginPageState extends State<LoginPage> {
+  var emailcontroller=TextEditingController();
+  var passwordcontroller=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login"),),
+      body: SafeArea(child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 40,),
+            TextField(
+              controller: emailcontroller,
+              
+              decoration: const InputDecoration(
+              hintText: "Email",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              )
+            ),
+            ),
+            const SizedBox(height: 20,),
+               TextField(
+                obscureText: true,
+                controller: passwordcontroller,
+
+                decoration: const InputDecoration(
+              hintText: "Password",
+               border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              )
+    
+            ),
+            
+            
+            ),
+            const SizedBox(height: 40,),
+            InkWell(
+              onTap: () {
+                Navigator.push(context,MaterialPageRoute(builder: (context) {
+                  return MyHomePage(title: emailcontroller.text);
+                }));
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width*0.5,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(255, 33, 149, 243),
+                ),
+                child: const Text("Login",style: TextStyle(
+                  color: Colors.white,
+                  
+                  fontWeight: FontWeight.bold
+                ),),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/Startpage.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:terainingapp2/Loginpage.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(
+        padding: const EdgeInsets.all(10),
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          color: Colors.red,
+          image: DecorationImage(
+              image: NetworkImage(
+                  "https://plus.unsplash.com/premium_photo-1680553492268-516537c44d91?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+              fit: BoxFit.cover),
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Hi,Welcome",style: TextStyle(fontSize: 60,fontWeight: FontWeight.bold,color: Colors.white)),
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.5,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const LoginPage();
+                  }));
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.8,
+                  height: 60,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Color.fromARGB(200, 33, 149, 243),
+                  ),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width*0.8,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color:Color.fromARGB(200, 33, 149, 243),
+                ),
+                child: const Text(
+                  "SignUp",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
